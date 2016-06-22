1b20882dc3b7215d9d11cd18e1f213491c5ddb68
Zack Yang <zack9433@gmail.com>
2016-06-22 16:58:46 +0800

    fix lint error

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -27,11 +27,11 @@ var config = {
   },
   module: {
     preLoaders: [
-      {test: /\.js$/, loader: "eslint", exclude: /(node_modules)/}
+      {test: /\.js$/, loader: 'eslint', exclude: /(node_modules)/}
     ],
     loaders: [
       { test: /\.js$/, loader: 'ng-annotate!babel?cacheDirectory', exclude: /(node_modules)/ },
-      { test: require.resolve("jquery"), loader: "expose?$!expose?jQuery" },
+      { test: require.resolve('jquery'), loader: 'expose?$!expose?jQuery' },
       { test: /\.json$/, loader: 'json', exclude: /node_modules/ },
       { test: /\.html$/, loader: 'ng-cache?prefix=[dir]/[dir]', exclude: /node_modules/ }
     ],
