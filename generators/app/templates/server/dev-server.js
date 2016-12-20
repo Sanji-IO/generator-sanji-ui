246903d341bb7f11aed583f6648ab5a94b5166b1
Zack Yang <zack9433@gmail.com>
2016-12-20 10:31:02 +0800

    modify

--- generators/app/templates/server/dev-server.js
+++ generators/app/templates/server/dev-server.js
@@ -3,9 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack.dev');
 new WebpackDevServer(webpack(config), {
   historyApiFallback: true,
-  stats: {
-    colors: true,
-  }
+  stats: 'minimal'
 })
 .listen(8080, '0.0.0.0', function(err) {
   if (err) {
