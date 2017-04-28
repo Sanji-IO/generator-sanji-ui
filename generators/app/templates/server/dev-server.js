4545c1d30b33bc397b1bfa815ac189088b2d2d12
Zack Yang <zack9433@gmail.com>
2017-04-28 16:30:01 +0800

    modify

--- generators/app/templates/server/dev-server.js
+++ generators/app/templates/server/dev-server.js
@@ -3,12 +3,12 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack.dev');
 new WebpackDevServer(webpack(config), {
   historyApiFallback: true,
-  stats: 'minimal'
-})
-.listen(8080, '0.0.0.0', function(err) {
+  stats: 'minimal',
+  disableHostCheck: true,
+  public: '0.0.0.0:8080'
+}).listen(8080, '0.0.0.0', function(err) {
   if (err) {
     throw err;
   }
   console.log('Listening at 0.0.0.0:8080');
 });
-
